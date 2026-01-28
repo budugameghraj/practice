3c2ce4a2c23e661fc175bf99ffb95823e8c2eefd
budugameghraj <meghr7734@gmail.com>
2026-01-28 19:32:57 +0000

    dict

--- apna college prac/student table.py
+++ apna college prac/student table.py
@@ -10,32 +10,19 @@ while Game == True:
     "'B' to update marks\n"
     "'C' to search student\n"
     "'D' to display the table\n" )
+    if action=="A":
+        name=set()
+        add_name=input("Enter student name : ")
+        name.add(add_name)
+        dict.update({name})
+        print(dict)
+        
 
-if action!="A" or "B" or "C" or "D":
-    Game=False
-new_name="Veer"
-marks=10
-dict.update({new_name:marks})
-print(dict)
+# new_name="Veer"
+# marks=10
+# dict.update({new_name:marks})
+# print(dict)
 
-up_name="Ram"
-dict.update({up_name:9})
-print(dict)
-
-search_name="Rahul"
-length=0
-index=0
-for i in dict:
-    length+=1
-for name in dict:
-    if name!=search_name:
-        index+=1
-    else:
-        break
-# if index<length-1:
-#     print(f"The student name is present at the index {index}.")
-# else:
-#     print("Student not present in the table")
-
-# if input=="D":
-#     print(dict)
+# up_name="Ram"
+# dict.update({up_name:9})
+# print(dict)
